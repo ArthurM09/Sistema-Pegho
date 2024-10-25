830a562461e107c2875d7e904cf477517221d1fe
Arthur Massuia Miranda <arthurm.massuia@gmail.com>
2024-10-25 14:54:17 -0300

    refazer

--- pegho/candidatos/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import DadosPessoais, Contato, ExperienciaProfissional, FormacaoAcademica
-
-admin.site.register(DadosPessoais)
-admin.site.register(Contato)
-admin.site.register(ExperienciaProfissional)
-admin.site.register(FormacaoAcademica)
