bee4b472bb86bfe30abe4572db55fa56b016fad6
Arthur Massuia Miranda <arthurm.massuia@gmail.com>
2024-10-30 10:47:02 -0300

    registro dos modelos

--- recrutamento/candidates/admin.py
+++ recrutamento/candidates/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import DadosPessoais, Contato, Experiencia, Formacao
 
-# Register your models here.
+@admin.register(DadosPessoais)
+class DadosPessoaisAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'data_nascimento', 'cpf')
+
+@admin.register(Contato)
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('dados_pessoais', 'email', 'telefone', 'endereco')
+
+@admin.register(Experiencia)
+class ExperienciaAdmin(admin.ModelAdmin):
+    list_display = ('dados_pessoais', 'cargo', 'empresa', 'entrada', 'saida', 'descricao')
+
+@admin.register(Formacao)
+class FormacaoAdmin(admin.ModelAdmin):
+    list_display = ('dados_pessoais', 'instituicao', 'curso', 'inicio', 'fim')
